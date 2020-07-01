009ab12a9d28df852e88e1b5b25e3fc97dd50ba7
deepa8115 <65079662+deepa8115@users.noreply.github.com>
2020-07-01 15:38:43 +0530

    Create Voting.cpp
    This program will check if person is eligible for voting or not by their age

---
+++ Voting.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int age;
+cout<<"enter your age: ";
+cin>>age;
+if(age>=18)
+{
+cout<<"person is eligible for voting"<<endl;
+}
+else
+{
+cout<<"person is not eligible for voting"<<endl;
+}
+return 0;
+}
